1f26371735d52039b5d904093a74b838d43d9616
matheus.rosa <matheus.rosa@alfaerp.com.br>
2022-08-26 14:04:55 -0300

    Adicionando validações de email e senha no login

---
+++ site/assets/js/login.js
@@ -0,0 +1,40 @@
+function validarLogin () {
+    let email = inputEmail.value;
+    let senha = inputSenha.value;
+    let cadastravel = 0;
+
+    if (email.indexOf('@') <= -1 || email.indexOf(".com") <= -1) {
+        spanEmail.innerHTML = "E-mail inválido.";
+    } else {
+        spanEmail.innerHTML = "";
+    }
+
+    if (senha.length == 0) {
+        spanSenha.innerHTML = "";
+        cadastravel = senha.length;
+    } else if (senha.length > 0 && senha.length <= 4) {
+        spanSenha.innerHTML = "Senha fraca.";
+        cadastravel = senha.length;
+    } else if (senha.length > 4 && senha.length <= 8) {
+        spanSenha.innerHTML = "Senha média.";
+    } else {
+        for (let i = 0; i <= 9; i++) {
+            if (senha.indexOf(i) <= -1) {
+                let caracEspecial = ["(", ")", "*", "&", "%", "$", "#", "@", "!", ";", "-", "_"];
+
+                let contemSpecial = false;
+
+                caracEspecial.forEach(special => {
+                    if (senha.indexOf(special) != -1) {
+                        spanSenha.innerHTML = "Senha muito forte.";
+                        contemSpecial = true;
+                    }
+                });
+
+                if (contemSpecial == false) {
+                    spanSenha.innerHTML = "Senha forte."
+                }
+            } 
+        }   
+    }  
+}
