9902783aa3adb3101a453fed91fcfad256e9c200
matheus-rosa11 <matheus.rosa@sptech.school>
2022-09-03 17:52:12 -0300

    Construindo funcionalidades de validação de Nome de Usuario, Email e Senha

---
+++ site/assets/js/validarCadastro.js
@@ -0,0 +1,65 @@
+function validarSenha() {
+
+    let senha = inputSenha.value;
+    let caracEspecial = ["(", ")", "*", "&", "%", "$", "#", "@", "!", ";", "-", "_"];
+    spanSenha.innerHTML = "";
+
+
+    if (senha.length == 0) {
+        spanSenha.innerHTML = "";
+    } else if (senha.length > 0 && senha.length <= 4) {
+        spanSenha.innerHTML = "Senha fraca.";
+    } else if (senha.length > 4 && senha.length <= 8) {
+        spanSenha.innerHTML = "Senha média.";
+    } else {
+    
+        let contemSpecial = false;
+    
+        for (let i = 0; i <= 9; i++) {
+            if (senha.indexOf(i) > -1) {
+    
+                caracEspecial.forEach(special => {
+                    if (senha.indexOf(special) != -1) {
+                        spanSenha.innerHTML = "Senha muito forte.";
+                        contemSpecial = true;
+                    }
+                });
+    
+                if (contemSpecial == false) {
+                    spanSenha.innerHTML = "Senha forte."
+                }
+            } 
+        }  
+    }  
+}
+
+function validarUsuario() {
+    let nomeUsuario = inputUsuario.value;
+    spanUsuario.indexHTML = "";
+    
+    if (nomeUsuario.length <= 1) {
+        spanUsuario.indexHTML = "Nome de usuário inválido";
+    } else {
+
+        for (let i = 0; i <= 9; i++) {
+            if (nomeUsuario.indexOf(i) != -1) {
+                spanUsuario.indexHTML = "Não são permitidos números.";
+            }
+
+            caracEspecial.forEach(special => {
+                if (nomeUsuario.indexOf(special) != -1) {
+                    spanUsuario.indexHTML = "Não são permitidos caracteres especiais."
+                }
+            });
+        }
+
+        
+    }
+}
+
+function validarEmail() {
+    let email = inputEmail.value;
+    spanEmail.indexHTML = "";
+
+    
+}
