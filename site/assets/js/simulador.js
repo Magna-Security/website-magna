a94179601605f74897e80b7aa9a8c2ad3cac495b
matheus.rosa <matheus.rosa@alfaerp.com.br>
2022-09-05 10:01:46 -0300

    Corrigindo bug evento no simulador

--- site/assets/js/simulador.js
+++ site/assets/js/simulador.js
@@ -1,8 +1,11 @@
-function simular(e) {
+let btn = document.querySelector(".algo");
+
+btn.addEventListener('click', (e) => {
     e.preventDefault();
+});
+
+function simular(e) {
     var formulario = document.querySelector('.formulario');
     formulario.classList.add('col-md-6');
     formulario.classList.remove('col-md-12');
-
-    
 }
