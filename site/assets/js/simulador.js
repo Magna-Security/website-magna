2a412523a2582f7db0dc4857d9d3e610018ab935
matheus.rosa <matheus.rosa@alfaerp.com.br>
2022-09-05 10:00:08 -0300

    Subindo template do simulador

---
+++ site/assets/js/simulador.js
@@ -0,0 +1,8 @@
+function simular(e) {
+    e.preventDefault();
+    var formulario = document.querySelector('.formulario');
+    formulario.classList.add('col-md-6');
+    formulario.classList.remove('col-md-12');
+
+    
+}
