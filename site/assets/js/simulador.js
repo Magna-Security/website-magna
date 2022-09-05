2df93827c46cd67023193d59ea328cb3d48d06c5
matheus.rosa <matheus.rosa@alfaerp.com.br>
2022-09-05 13:35:28 -0300

    Subindo modificações no index e simulador financeiro

--- site/assets/js/simulador.js
+++ site/assets/js/simulador.js
@@ -15,64 +15,75 @@ function bt1() {
     var ax_so = (document.getElementById('so').value);
     var ax_func = Number(document.getElementById('func').value);
 
-    if (ax_so == "linux") {
-        
-        if (ax_alunos < 1000) {
-            document.getElementById('spanText').innerHTML = `Você normalmente gastaria R$: ${(2801.2331).toFixed(2)}  mensalmente \n
-            adquirindo nosso sistema você poderia gastar ${(2801.2331 * 0.80).toFixed(2)} em meses normais 
-            é ${(2801.2331 * 1.05).toFixed(2)} em meses de prova\n
-            você economezaria R$:${(  2801.2331 - (2801.2331 * 0.80) ).toFixed(2)} em meses normais  `
-        } else if (ax_alunos < 2000 && ax_alunos >= 1000){
+    if (ax_alunos == "" || ax_so == "" || ax_func == "") {
+        alert('Valores inválidos.');
+    } else {
 
-        } else if (ax_alunos < 3000 && ax_alunos >= 2000){
+        simular();
 
-        } else if( ax_alunos < 5000 && ax_alunos >= 3000){ 
+        document.querySelector('.resultado').style.display = 'block';
 
-        } else if (ax_alunos < 7000 && ax_alunos >= 5000){
-
-        } else if (ax_alunos < 10000 && ax_alunos >= 7000){
-
-        } else if (ax_alunos < 15000 && ax_alunos >= 10000){
-
-        } else if (ax_alunos < 20000 && ax_alunos >= 15000){
-
-        } else if (ax_alunos < 30000 && ax_alunos >= 20000){
-
-        } else if (ax_alunos < 40000 && ax_alunos >= 30000) {
-
-        }else if (ax_alunos < 50000 && ax_alunos  >= 40000){
-
-        }else{
-
-        }
-    } else if (ax_so == "windowns") {
-
-        if (ax_alunos < 1000) {
+        if (ax_so == "linux") {
             
-        } else if (ax_alunos < 2000 && ax_alunos >= 1000){
-
-        } else if (ax_alunos < 3000 && ax_alunos >= 2000){
-
-        } else if( ax_alunos < 5000 && ax_alunos >= 3000){ 
-
-        } else if (ax_alunos < 7000 && ax_alunos >= 5000){
-
-        } else if (ax_alunos < 10000 && ax_alunos >= 7000){
-
-        } else if (ax_alunos < 15000 && ax_alunos >= 10000){
-
-        } else if (ax_alunos < 20000 && ax_alunos >= 15000){
-
-        } else if (ax_alunos < 30000 && ax_alunos >= 20000){
-
-        } else if (ax_alunos < 40000 && ax_alunos >= 30000) {
-
-        }else if (ax_alunos < 50000 && ax_alunos  >= 40000){
-
-        }else{
-
+            if (ax_alunos < 1000) {
+                document.getElementById('spanText').innerHTML = `Você normalmente gastaria R$: ${(2801.2331).toFixed(2)}  mensalmente \n
+                adquirindo nosso sistema você poderia gastar ${(2801.2331 * 0.80).toFixed(2)} em meses normais 
+                é ${(2801.2331 * 1.05).toFixed(2)} em meses de prova\n
+                você economezaria R$:${(  2801.2331 - (2801.2331 * 0.80) ).toFixed(2)} em meses normais  `
+            } else if (ax_alunos < 2000 && ax_alunos >= 1000){
+    
+            } else if (ax_alunos < 3000 && ax_alunos >= 2000){
+    
+            } else if( ax_alunos < 5000 && ax_alunos >= 3000){ 
+    
+            } else if (ax_alunos < 7000 && ax_alunos >= 5000){
+    
+            } else if (ax_alunos < 10000 && ax_alunos >= 7000){
+    
+            } else if (ax_alunos < 15000 && ax_alunos >= 10000){
+    
+            } else if (ax_alunos < 20000 && ax_alunos >= 15000){
+    
+            } else if (ax_alunos < 30000 && ax_alunos >= 20000){
+    
+            } else if (ax_alunos < 40000 && ax_alunos >= 30000) {
+    
+            }else if (ax_alunos < 50000 && ax_alunos  >= 40000){
+    
+            }else{
+    
+            }
+        } else if (ax_so == "windowns") {
+    
+            if (ax_alunos < 1000) {
+                
+            } else if (ax_alunos < 2000 && ax_alunos >= 1000){
+    
+            } else if (ax_alunos < 3000 && ax_alunos >= 2000){
+    
+            } else if( ax_alunos < 5000 && ax_alunos >= 3000){ 
+    
+            } else if (ax_alunos < 7000 && ax_alunos >= 5000){
+    
+            } else if (ax_alunos < 10000 && ax_alunos >= 7000){
+    
+            } else if (ax_alunos < 15000 && ax_alunos >= 10000){
+    
+            } else if (ax_alunos < 20000 && ax_alunos >= 15000){
+    
+            } else if (ax_alunos < 30000 && ax_alunos >= 20000){
+    
+            } else if (ax_alunos < 40000 && ax_alunos >= 30000) {
+    
+            }else if (ax_alunos < 50000 && ax_alunos  >= 40000){
+    
+            }else{
+    
+            }
         }
     }
+    
+    
 
 
 }
