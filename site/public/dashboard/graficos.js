cae44a7a6629e4ab0adfd975f28bc63f0b9ca1ef
MatheusSFelixx <matheus.felix@sptech.school>
2022-09-07 19:34:07 -0300

    dash e valores

--- site/public/dashboard/graficos.js
+++ site/public/dashboard/graficos.js
@@ -8,8 +8,8 @@ const data = {
   datasets: [
     {
       label: "My First dataset",
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: ["#576E97"],
+      borderColor: ["#68BECB", "#576E97", "#578E97"],
       data: [0, 10, 5, 2, 20, 30, 45],
     },
   ],
@@ -39,24 +39,8 @@ const data2 = {
     {
       label: "My First Dataset",
       data: [65, 59, 80, 81, 56, 55, 40],
-      backgroundColor: [
-        "rgba(255, 99, 132)",
-        "rgba(255, 159, 64)",
-        "rgba(255, 205, 86)",
-        "rgba(75, 192, 192)",
-        "rgba(54, 162, 235)",
-        "rgba(153, 102, 255)",
-        "rgba(201, 203, 207)",
-      ],
-      borderColor: [
-        "rgb(255, 99, 132)",
-        "rgb(255, 159, 64)",
-        "rgb(255, 205, 86)",
-        "rgb(75, 192, 192)",
-        "rgb(54, 162, 235)",
-        "rgb(153, 102, 255)",
-        "rgb(201, 203, 207)",
-      ],
+      backgroundColor: ["#68BECB", "#576E97", "#578E97"],
+      borderColor: ["rgb(201, 203, 207)"],
       borderWidth: 1,
     },
   ],
@@ -77,16 +61,12 @@ const config2 = {
 // Gráfico 3 =====================================================================
 
 const data3 = {
-  labels: ["Red", "Blue", "Yellow"],
+  labels: ["1", "2", "3"],
   datasets: [
     {
       label: "My First Dataset",
       data: [300, 50, 100],
-      backgroundColor: [
-        "rgb(255, 99, 132)",
-        "rgb(54, 162, 235)",
-        "rgb(255, 205, 86)",
-      ],
+      backgroundColor: ["#68BECB", "#576E97", "#578E97"],
       hoverOffset: 4,
     },
   ],
