07fef7e841d6d05f72458c39fb044def615c8f37
MatheusSFelixx <matheus.felix@sptech.school>
2022-11-13 20:19:29 -0300

    atualização dos graficos de gerente

--- site/public/dashboard/js/geral-dash.js
+++ site/public/dashboard/js/geral-dash.js
@@ -9,8 +9,10 @@ if (sessionStorage.getItem("cargo") == "suporte") {
 }
 
 if (sessionStorage.getItem("cargo") == "tecnico") {
+  arrayLinhas[0].classList.add("invisivel");
+  arrayLinhas[2].classList.add("invisivel");
   // arrayLinhas[0].classList.add("invisivel");
-  // arrayLinhas[2].classList.add("invisivel");
+  // arrayLinhas[1].classList.add("invisivel");
 }
 
 if (sessionStorage.getItem("cargo") == "gerente") {
